8e7843dd386a6e0453f2bb92db97ad191236da2e
Paul Soucy <paul.soucy@videray.com>
2019-02-11 13:04:41 -0500

    wip

--- src/main/groovy/com/devsmart/crossbuild/plugins/cmake/CrossBuildCMakeLibPlugin.groovy
+++ src/main/groovy/com/devsmart/crossbuild/plugins/cmake/CrossBuildCMakeLibPlugin.groovy
@@ -5,6 +5,7 @@ import com.devsmart.crossbuild.BuildTarget
 import com.devsmart.crossbuild.DefaultPublishableComponent
 import com.devsmart.crossbuild.plugins.CrossBuildExtention
 import com.devsmart.crossbuild.plugins.CrossBuildPlugin
+import com.devsmart.crossbuild.plugins.TargetConfig
 import com.devsmart.crossbuild.tasks.BuildCMakeTask
 import com.devsmart.crossbuild.tasks.ConfigCMakeTask
 import org.gradle.api.Plugin
@@ -22,7 +23,6 @@ class CrossBuildCMakeLibPlugin implements Plugin<Project> {
         CrossBuildExtention crossBuildExt = project.extensions.getByName(CrossBuildPlugin.EXTENSION_NAME)
 
         CMakeLibrarySpec mainLib = project.extensions.create(EXTENTION, CMakeLibrarySpec.class)
-
         mainLib.whenConfigured {
 
             mainLib.component = new MainCMakeLibraryComponent()
@@ -30,52 +30,53 @@ class CrossBuildCMakeLibPlugin implements Plugin<Project> {
             project.components.add(mainLib.component)
 
             crossBuildExt.targets.all {
-
-                def targetInfo = delegate
-
-                mainLib.component.addVariant(new DefaultPublishableComponent.Builder(project)
-                        .withGroup(project.group)
-                        .withProjectName(project.name)
-                        .withVersion(project.version)
-                        .withBuildTarget(targetInfo.buildTarget(project.objects))
-                        .withVariantName('debug')
-                        .build())
-
-                String comboName = String.format('%s%s%s',
-                        project.name.capitalize(), targetInfo.os.capitalize(), targetInfo.arch.capitalize())
-
-                File installDir = project.file("build/${comboName}/install")
-
-                def configTask = project.tasks.register("config${comboName}", ConfigCMakeTask) {
-                    srcDir = mainLib.srcDir
-                    buildDir = project.file("build/cmake/${comboName}")
-                    generator = 'Ninja'
-                    cmakeArgs = [targetInfo.cmakeArgs, mainLib.cmakeArgs, "CMAKE_INSTALL_PREFIX=${installDir.absolutePath}"].flatten()
-
-                }
-
-                def buildTask = project.tasks.register("assemble${comboName}", BuildCMakeTask) {
-                    group = 'build'
-                    generatedBy(configTask.get())
-                    buildOutputs = project.fileTree(dir: buildDir, include: '**/*')
-                }
-
-                def installTask = project.tasks.register("install${comboName}", BuildCMakeTask) {
-                    group = 'Install'
-                    target = 'install'
-                    generatedBy(configTask.get())
-                    buildOutputs = project.fileTree(dir: installDir, include: '**/*')
-                    dependsOn(buildTask.get())
-                }
-
+                addVariants(project, mainLib, it)
             }
         }
 
+    }
 
+    private void addVariants(Project project, CMakeLibrarySpec mainLib, TargetConfig target) {
+        addNamedVariant(project, mainLib, target, 'debug')
+        addNamedVariant(project, mainLib, target, 'release')
+    }
 
+    private void addNamedVariant(Project project, CMakeLibrarySpec mainLib, TargetConfig targetCfg, String variantName) {
 
+        mainLib.component.addVariant(new DefaultPublishableComponent.Builder(project)
+                .withGroup(project.group)
+                .withProjectName(project.name)
+                .withVersion(project.version)
+                .withBuildTarget(targetCfg.buildTarget(project.objects))
+                .withVariantName(variantName)
+                .build())
 
+        String comboName = String.format('%s%s%s%s',
+                project.name.capitalize(), variantName.capitalize(), targetCfg.os.capitalize(), targetCfg.arch.capitalize())
 
+        File installDir = project.file("build/${comboName}/install")
+
+        ConfigCMakeTask configTask = project.tasks.create("config${comboName}", ConfigCMakeTask) {
+            srcDir = mainLib.srcDir
+            buildDir = project.file("build/cmake/${comboName}")
+            generator = 'Ninja'
+            cmakeArgs = ["CMAKE_BUILD_TYPE=${variantName}", targetCfg.cmakeArgs, mainLib.cmakeArgs, "CMAKE_INSTALL_PREFIX=${installDir.absolutePath}"].flatten()
+
+        }
+
+        BuildCMakeTask buildTask = project.tasks.create("assemble${comboName}", BuildCMakeTask) {
+            group = 'build'
+            generatedBy(configTask)
+            buildOutputs = project.fileTree(dir: configTask.buildDir, include: '**/*')
+        }
+
+        BuildCMakeTask installTask = project.tasks.create("install${comboName}", BuildCMakeTask) {
+            group = 'Install'
+            target = 'install'
+            generatedBy(configTask)
+            buildOutputs = project.fileTree(dir: installDir, include: '**/*')
+            dependsOn(buildTask)
+        }
     }
 
 }
